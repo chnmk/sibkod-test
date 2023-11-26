850f60282c431c3c6d041b02be08c02d98aace5c
chnmk <a2molchanov@ya.ru>
2023-11-26 12:32:16 +0300

    npm create vue@latest

---
+++ src/components/output.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Hello world!
+        {{ jsonRaw }}
+    </div>
+</template>
+
+<script setup>
+import jsonRaw from "../users/data.json"
+console.log(jsonRaw)
+</script>
